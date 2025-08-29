ba70df10bc803f2b1f948d85eb687f2ac6c07aec
YashGupta-24 <yashgupta.241103@gmail.com>
2025-08-29 22:06:27 +0530

    I/O update

--- fake-news-detector-backend/start.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    subprocess.Popen(["poetry", "run", "python", "Agents/agent.py"])
-    subprocess.run(["poetry", "run", "python", "Server/rest.py"])
